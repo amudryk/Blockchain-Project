fe1789d74f2dc41abb91f2f5f6fb24c53d992947
Adam Mudryk <40345201+amudryk@users.noreply.github.com>
2018-06-22 16:19:18 -0400

    Node Class, Hash & Verification Functions

---
+++ hash.py
@@ -0,0 +1,7 @@
+import hashlib
+import json
+
+def hash_block(block):
+    hashable_block = block.__dict__.copy()
+    hashable_block['transactions'] = [tx.ordered_dict() for tx in hashable_block['transactions']]
+    return hashlib.sha256(json.dumps(hashable_block, sort_keys=True).encode()).hexdigest()
