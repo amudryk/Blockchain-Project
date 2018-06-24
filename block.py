c2eb4ac29128d86158f92e8646b4f5252e4d1027
Adam Mudryk <40345201+amudryk@users.noreply.github.com>
2018-06-24 19:40:33 -0400

    Update block.py
    Added index

--- block.py
+++ block.py
@@ -1,11 +1,12 @@
 from time import time
 
 class Block:
-    def __init__(self, previous_hash, transactions, proof, timestamp=None):
+    def __init__(self, previous_hash, index, transactions, proof, timestamp=None):
         self.previous_hash = previous_hash
         self.timestamp = time() if timestamp is None else timestamp
         self.transactions = transactions
         self.proof = proof
+        self.index = index
     
     def __repr__(self):
-        return 'Previous Hash: {}, Proof: {}, Transactions: {} '.format(self.previous_hash, self.proof, self.transactions)
+        return 'Previous Hash: {}, Index: {}, Proof: {}, Transactions: {} '.format(self.previous_hash, self.index, self.proof, self.transactions)
