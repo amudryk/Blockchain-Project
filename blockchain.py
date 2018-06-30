1dbb6f1aef3f4eee43d7d21fc9f00ed5517c3e46
Adam Mudryk <40345201+amudryk@users.noreply.github.com>
2018-06-30 13:44:16 -0400

    Update blockchain.py

--- blockchain.py
+++ blockchain.py
@@ -1,151 +1,180 @@
-import functools 
-import hashlib
-import json
-import collections
-#import pickle
+from flask import Flask, jsonify, request, send_from_directory
+from flask_cors import CORS
 
-from hash import hash_block
-from block import Block
-from transaction import Transaction
 from wallet import Wallet
-import verification
+from blockchain import Blockchain
 
-MINING_REWARD = 10
+app = Flask(__name__)
+CORS(app)
 
-class Blockchain:
-    def __init__(self, hosting_node_id):
-        genesis_block = Block('', [], 99, 0)
-        self.__chain = [genesis_block]
-        self.__open_transactions = []
-        self.__peer_nodes = set()
-        self.load_data()
-        self.hosting_node = hosting_node_id
+@app.route('/', methods=['GET'])
+def get_ui():
+    return send_from_directory('ui', 'node.html')
+
+@app.route('/network', methods=['GET'])
+def get_network_ui():
+    return send_from_directory('ui', 'network.html')
+
+@app.route('/wallet', methods=['POST'])
+def create_keys():
+    wallet.create_keys()
+    if wallet.save_keys():
+        global blockchain
+        blockchain = Blockchain(wallet.public_key, port)
+        response = {
+            'public_key': wallet.public_key,
+            'private_key': wallet.private_key,
+            'funds': blockchain.get_balance()
+        }
+        return jsonify(response), 201
+    else:
+        response = {'message': 'Saving keys failed.'}
+        return jsonify(response), 500
         
+@app.route('/wallet', methods=['GET'])
+def load_keys():
+    if wallet.load_keys():
+        global blockchain
+        blockchain = Blockchain(wallet.public_key, port)
+        response = {
+            'public_key': wallet.public_key,
+            'private_key': wallet.private_key,
+            'funds': blockchain.get_balance() 
+        }
+        return jsonify(response), 201
+    else:
+        response = {'message': 'Loading keys failed.'}
+        return jsonify(response), 500
 
-    def get_chain(self):
-        return self.__chain[:]
+@app.route('/balance', methods=['GET'])
+def get_balance():
+    balance = blockchain.get_balance()
+    if balance != None:
+        response = {
+            'message': 'Fetched balance successfully.',
+            'funds': balance
+        }
+        return jsonify(response), 200
+    else:
+        response = {
+            'message': 'Loading balance failed.',
+            'wallet_set_up': wallet.public_key != None
+        }
+        return jsonify(response), 500
+
+@app.route('/transaction', methods=['POST'])
+def add_transaction():
+    if wallet.public_key == None:
+        response = {'message': 'No wallet' }
+        return jsonify(response), 400
+
+    values = request.get_json()
+    if not values:
+        response = {'message': 'No data found.'}
+        return jsonify(response), 400
     
-    def get_open_transactions(self):
-        return self.__open_transactions[:]
-
-    def add_transaction(self, recipient, hosting_node, signature, amount=1.0):
-        if self.hosting_node == None:
-            return False
-        
-        #transaction = {'sender':sender, 'recipient': recipient, 'amount': amount}
-        transaction = Transaction(hosting_node, recipient, signature, amount)
-        if verification.verify_transaction(transaction, self.get_balance):
-            self.__open_transactions.append(transaction) 
-            self.save_data()
-            return True 
-        return False
+    required_fields = ['recipient', 'amount']
     
-    def mine_block(self):
-        if self.hosting_node == None:
-            return None
-        
-        last_block = self.__chain[-1]
-        previous_hash = hash_block(last_block)
-        proof = self.proof_of_work()
-        
+    if not all(field in values for field in required_fields):
+        response = {'message': 'Required data is missing.'}
+        return jsonify(response), 400
 
-        #reward_transaction = {'sender':'MINING', 'recipient': owner, 'amount': MINING_REWARD}
-        reward_transaction = Transaction('MINING', self.hosting_node, '', MINING_REWARD)
-        
-        copied_transactions = self.__open_transactions[:]
-        
-        for tx in copied_transactions:
-            if not Wallet.verify_transaction(tx):
-                return None
-        
-        copied_transactions.append(reward_transaction)
-        mined_block = Block(previous_hash, copied_transactions, proof)
-     
-        self.__chain.append(mined_block)
-        self.__open_transactions = []
-        self.save_data()
-        return mined_block
+    recipient = values['recipient']
+    amount = values['amount']
+    signature = wallet.sign_transaction(wallet.public_key, recipient, amount)
+    success = blockchain.add_transaction(recipient, wallet.public_key, signature, amount)
+    if success:
+        response = {
+            'message': 'Successfully added transaction.',
+            'transaction': {
+                'sender': wallet.public_key,
+                'recipient': recipient,
+                'amount': amount,
+                'signature': signature
+            },
+            'funds': blockchain.get_balance()
+        }
+        return jsonify(response, 201)
+    else:
+        response = {'message': 'Creating a transaction failed.'}
+        return jsonify(response), 500
 
-    def get_balance(self):
-        if self.hosting_node == None:
-            return None
-        
-        participant = self.hosting_node
-        sender = [[tx.amount for tx in block.transactions if tx.sender == participant] for block in self.__chain]
-        open_tx_sender = [tx.amount for tx in self.__open_transactions if tx.sender == participant]
-        sender.append(open_tx_sender)
-        amount_sent = functools.reduce(lambda tx_sum, tx_amt: tx_sum + sum(tx_amt) if len(tx_amt) > 0 else tx_sum + 0, sender, 0)
+@app.route('/mine', methods=['POST'])
+def mine():
+    mined_block = blockchain.mine_block()
     
-        recipient = [[tx.amount for tx in block.transactions if tx.recipient == participant] for block in self.__chain]
-        amount_recieved = functools.reduce(lambda tx_sum, tx_amt: tx_sum + sum(tx_amt) if len(tx_amt) > 0 else tx_sum + 0, recipient, 0 )
+    if mined_block != None:
+        dict_block = mined_block.__dict__.copy()
+        dict_block['transactions'] = [tx.__dict__ for tx in dict_block['transactions']]
+        response = {
+            'message': 'Block added successfully.',
+            'block': dict_block,
+            'funds': blockchain.get_balance()
+        }
+        return jsonify(response), 201
+    else:
+        response = {
+            'message': 'Adding a block failed.',
+            'wallet_set_up': wallet.public_key != None
+        }
+        return jsonify(response), 500
+
+@app.route('/chain', methods=['GET'])
+def get_chain():
+    chain_snapshot = blockchain.get_chain()
+    dict_chain = [block.__dict__.copy() for block in chain_snapshot]
+    for dict_block in dict_chain:
+        dict_block['transactions'] = [tx.__dict__ for tx in dict_block['transactions']]
+    return jsonify(dict_chain), 200
+
+@app.route('/open-transactions', methods=['GET'])
+def get_open_transaction():
+    transactions = blockchain.get_open_transactions()
+    dict_transactions = [tx.__dict__ for tx in transactions]
+    return jsonify(dict_transactions), 200
+
+@app.route('/nodes', methods=['POST'])
+def add_node():
+    values = request.get_json()
+    if not values:
+        response = {'message': 'No data attached.'}
+        return jsonify(response), 400
+    if 'node' not in values:
+        response = {'message': 'No node data.'}
+        return jsonify(response), 400
+    node = values['node']
+    blockchain.add_peer_node(node)
+    response = {
+        'message': 'Node added successfully.',
+        'node_list': blockchain.get_peer_nodes()
+    }
+    return jsonify(response), 201
+
+# input ex./node/localhost:1000
+@app.route('/nodes/<node_url>', methods=['DELETE'])
+def remove_node(node_url):
+    if node_url == '' or node_url == None:
+        response = {'message': 'No node data'}
+        return jsonify(response), 400
+    blockchain.remove_peer_node(node_url)
+    response = {
+        'message': 'Node removed successfully.',
+        'node_list': blockchain.get_peer_nodes()
+    }
+    return jsonify(response), 200
+
+@app.route('/nodes', methods=['GET'])
+def get_node():
+    nodes = blockchain.get_peer_nodes()
+    response = {'nodes_list': nodes}
+    return jsonify(response), 200
     
-        return amount_recieved - amount_sent
-
-    def load_data(self):
-
-        try:
-            with open ('blockchain.txt', mode='r') as f:
-                #file_content = pickle.loads(file.read())
-                #blockchain = file_content['chain']
-                #open_transactions = file_content['ot']
-                
-                file_content = f.readlines()
-                
-                blockchain = json.loads(file_content[0][:-1])
-                updated_blockchain = []
-                for block in blockchain:
-                    converted_tx = [Transaction(tx['sender'], tx['recipient'], tx['signature'], tx['amount']) for tx in block['transactions']]
-                    updated_block = Block(block['previous_hash'], converted_tx, block['proof'], block['timestamp'])
-                    updated_blockchain.append(updated_block)
-                self.__chain = updated_blockchain
-                
-                open_transactions = json.loads(file_content[1][:-1])
-                updated_transactions = []
-                for tx in open_transactions:
-                    updated_transaction = Transaction(tx['sender'], tx['recipient'], tx['signature'], tx['amount'])
-                    updated_transactions.append(updated_transaction)
-                self.__open_transactions = updated_transactions
-
-                peer_nodes = json.loads(file_content[2])
-                self.__peer_nodes = set(peer_nodes)
-        except (IOError,IndexError):
-            pass
-
-    
-    def save_data(self):
-        try:
-            with open('blockchain.txt', mode='w') as f:
-                saveable_chain = [block.__dict__ for block in [Block(block_el.previous_hash, [tx.__dict__ for tx in block_el.transactions], block_el.proof, block_el.timestamp) for block_el in self.__chain]]
-                f.write(json.dumps(saveable_chain))
-                f.write('\n')
-                saveable_otx = [tx.__dict__ for tx in self.__open_transactions]
-                f.write(json.dumps(saveable_otx))
-                f.write('\n')
-                f.write(json.dumps(list(self.__peer_nodes)))
-                
-                #pickle(binary) code
-                #save_data = { 'chain': blockchain, 'ot': open_transactions }
-                #file.write(pickle.dumps(save_data))
-        except IOError:
-            print('Saving failed!')
-
-    def proof_of_work(self):
-        last_block = self.__chain[-1]
-        last_hash = hash_block(last_block)
-        proof = 0
-        while not verification.valid_proof(self.__open_transactions, last_hash, proof):
-            proof += 1
-        return proof
-
-    def add_peer_node(self, node):
-        self.__peer_nodes.add(node)
-        self.save_data()
-
-    def remove_peer_node(self, node):
-        self.__peer_nodes.discard(node)
-        self.save_data()
-
-    def get_peer_nodes(self):
-        return list(self.__peer_nodes)  
-
-
+if __name__ == '__main__':
+    from argparse import ArgumentParser
+    parser = ArgumentParser()
+    parser.add_argument('-p', '--port', type=int, default = 9999)
+    args = parser.parse_args()
+    port = args.port
+    wallet = Wallet(port)
+    blockchain = Blockchain(wallet.public_key, port)
+    app.run(host='0.0.0.0', port=port)
