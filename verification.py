ada8c2acc7c26ef22b6562d20a4ad87c9d802209
Adam Mudryk <40345201+amudryk@users.noreply.github.com>
2018-07-05 18:55:44 -0400

    Update verification.py

--- verification.py
+++ verification.py
@@ -2,6 +2,7 @@ import hashlib
 from hash import hash_block
 from wallet import Wallet
 
+
 def verify_chain(blockchain):
     for (index, block) in enumerate(blockchain):
         if index > 0:
@@ -10,19 +11,22 @@ def verify_chain(blockchain):
             if not valid_proof(block.transactions[:-1], block.previous_hash, block.proof):
                 print('Proof of work is invalid')
                 return False
-    return True  
-    
+    return True
+
+
 def verify_transaction(transaction, get_balance, check_funds=True):
     if check_funds:
         sender_balance = get_balance(transaction.sender)
         return sender_balance >= transaction.amount and Wallet.verify_transaction(transaction)
-    else: 
+    else:
         return Wallet.verify_transaction(transaction)
+
 
 def verify_transactions(open_transactions, get_balance):
     return all([verify_transaction(tx, get_balance, False) for tx in open_transactions])
 
+
 def valid_proof(transactions, last_hash, proof):
     guess = (str([tx.ordered_dict() for tx in transactions]) + str(last_hash) + str(proof)).encode()
     guess_hash = hashlib.sha256(guess).hexdigest()
-    return guess_hash[0:2] == '99'  
+    return guess_hash[0:2] == '99'
